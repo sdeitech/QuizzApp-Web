f8c7527d66c8397fa8da3dde9b4c0c2794ae1131
Gohel Snehal <snehal.gohel@xongolab.com>
2021-05-04 20:51:11 +0530

    manage plan bug resolved

--- src/views/plans/view.js
+++ src/views/plans/view.js
@@ -183,7 +183,7 @@ class Plans extends Component {
                                             return <div class="col-lg-4 col-md-6 col-sm-6">
                                                 <div class={"main_plan "+ val1.subscriptionCode.toLowerCase()}>
                                                     <h3>{val1.title}</h3>
-                                                    <h5>${val1.price}</h5>
+                                                    <h5>{(val1.price > 0) ? '$'+val1.price : 'Free' }</h5>
                                                     <p>{val1.description}</p>
                                                     {(val1.isActive === false && val1.subscriptionCode !== 'BASIC') ? <button type="button" onClick={this.handlePurcaseClick.bind(this,val1._id)}>Purchase</button> : null}
                                                     {(val1.subscriptionCode === 'BASIC') ? <button type="button">Free</button> : null}
